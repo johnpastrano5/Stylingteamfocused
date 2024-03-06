83c22d287e92b5d35fa3170d1db83e614a15c0cb
johnpastrano5 <johnpastrano5@gmail.com>
2024-03-06 21:11:18 +0800

    Team-Focused-Styling

---
+++ src/components/Contents.js
@@ -0,0 +1,13 @@
+import { View, StyleSheet, Keyboard, Alert} from 'react-native'
+import { TextInput, Button } from 'react-native-paper'
+import React from 'react'
+
+const Contents = () => {
+  return (
+    <View>
+      <Text>Contents</Text>
+    </View>
+  )
+}
+
+export default Contents
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import { View, Text,  Image, StyleSheet } from 'react-native'
+import React from 'react'
+import TF from '../../assets/TF.webp'
+
+const Header = () => {
+  return (
+    <View style= {styles.header}>
+        <Image source={TF} style={styles.logoheader}/>
+      <Text style = {styles.HeaderTitle}>Team Focused</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 150,
+        paddingTop: 36,
+        backgroundColor: '#08313A',
+        alignItems: 'center',
+        
+
+    },
+    HeaderTitle: {
+      color:'white',
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+      fontsize: '64'
+    },
+    logoheader: {
+      width: 80,
+      height: 80
+    }
+  });
+
+export default Header
